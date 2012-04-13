259196a3dd47cb4221ace003ffcc0c28c82a19c8
David Trail <napalm@squareownz.org>
2012-04-13 20:05:40 +0200

    Finalised tv.py, all is working now

--- tv.py
+++ tv.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python
 
-import pprint
 import psycopg2 as pg
 import json
+from collections import OrderedDict
+from decimal import Decimal
+from datetime import datetime
 
-conn = pg.connect("dbname=napalm user=postgres")
+import cgitb
+cgitb.enable()
+
+conn = pg.connect("dbname=napalm user=lighty password=niggah")
 
 cur=conn.cursor()
 
-cur.execute('''SELECT * FROM tv_view''')
-shows = cur.fetchall()
-
-pprint.pprint(shows)
-
+cur.execute('''SELECT name, season, episode, rating, view_date FROM tv_view order by name, season, episode, id''')
+shows_array = cur.fetchall()
 
 cur.close()
 conn.close()
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        elif isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
+shows = []
+for show in shows_array:
+    tmp = {}
+    tmp['name'] = show[0]
+    tmp['season'] = show[1]
+    tmp['episode'] = show[2]
+    tmp['rating'] = show[3]
+    tmp['view_date'] = show[4]
+    shows.append(tmp)
+
+print json.dumps(shows, cls=ComplexEncoder)
